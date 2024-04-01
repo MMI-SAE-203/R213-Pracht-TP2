1c89fc48c584ef2f35411c1ccf035409aec79be0
pierre pracht <pierre.pracht@gmail.com>
2024-04-01 13:34:09 +0200

    ajout onErrorCaptured dans App.vue
    fixup! log error

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { onErrorCaptured } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
+
+onErrorCaptured((err, instance, info) => {
+  console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
+  return true
+})
 </script>
 
 <template>
